4388718bc57fb8c7c2f6ecba1b50eb502efc5d1a
aleks-123 <ace.ki@hotmail.com>
2023-07-04 17:41:41 +0200

    284

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,25 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENETS = [
-  { id: "1d", event: "Rock festival" },
-  { id: "2d", event: "Rap festival" },
-  { id: "3d", event: "Kpop festival" },
-  { id: "4d", event: "Mak muzika" },
-];
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 const EventsPage = () => {
+  const events = useLoaderData();
+
   return (
-    <div>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENETS.map((even) => (
-          <li key={even.id}>
-            <Link to={even.id}>{even.event}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <EventsList events={events} />
+    </>
   );
 };
 
